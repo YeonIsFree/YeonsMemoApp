45bafa6f238a335194a6d618d615dd25a12a7f96
YeonIsFree <seryun91@naver.com>
2022-11-06 19:20:16 +0900

    Initial Commit

---
+++ YeonsMemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YeonsMemoApp
+//
+//  Created by GOUN BAEK on 2022/11/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
